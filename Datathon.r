cc3815b5398bf17548655b8e190e087b13c79cce
Johnny Young <jzyoung@umich.edu>
2018-04-13 09:37:39 -0400

    import data

--- Datathon.r
+++ Datathon.r
@@ -1,9 +1,24 @@
 library(dplyr)
 library(magrittr)
 
-# Import Data
+# Import Main Data
 data <- read.csv('PRIMARY_flight_traffic.csv')
 
 # Clean Data
 data$diverted %<>% as.factor()
 data$cancelled %<>% as.factor()
+
+# Fare data 
+fares <- read.csv('fares.csv')
+
+# Stock data
+stock <- read.csv('stock_prices.csv')
+
+# Airport data
+airport <- read.csv('airports.csv')
+
+# Event data
+events <- read.csv('events_US.csv')
+
+# Weather data
+weather <- read.csv('weather.csv')
