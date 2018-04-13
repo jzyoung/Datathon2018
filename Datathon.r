cd91311f2eefbe0ef6dc362f34e54cdfe93bdb9c
Johnny Young <jzyoung@umich.edu>
2018-04-13 12:50:46 -0400

    avg price

--- Datathon.r
+++ Datathon.r
@@ -11,6 +11,11 @@ data$cancelled %<>% as.factor()
 # Fare data 
 fares <- read.csv('fares.csv')
 
+costs <- (as.numeric(substring(colnames(fares)[6:255], 2)) + 5) 
+fares$avgPrice <- apply(fares[,6:255],1, function(x){sum(x*costs)/sum(x)})
+# head(fares$avgPrice)
+
+
 # Stock data
 stock <- read.csv('stock_prices.csv')
 
@@ -22,3 +27,5 @@ events <- read.csv('events_US.csv')
 
 # Weather data
 weather <- read.csv('weather.csv')
+
+611, 2232
