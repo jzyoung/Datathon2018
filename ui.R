1fbe86b8d5e7baeaa0fa52f66254cfad65e1866f
Johnny Young <jzyoung@umich.edu>
2018-04-13 15:16:34 -0400

    EMOJI

--- ui.R
+++ ui.R
@@ -1,5 +1,7 @@
 
 library(shiny)
+library(emojifont)
+
 airlines <- c("AA" ,"AS" ,"B6" ,"DL" ,"EV" ,"F9" ,"HA" ,"NK" ,"OO" ,"UA" ,"VX" ,"WN")
 
 fluidPage(
@@ -12,10 +14,12 @@ fluidPage(
       sidebarPanel(
         selectInput("DepAP", "Departure Airport", sort(airports$airport_name)),
         selectInput("ArrAP", "Arrival Airport", sort(airports$airport_name)),
-        selectInput("Var", "Metric", c("elapsed", "fares", "delay")),
+        selectInput("Var", "Metric", c("Elapsed", "Fares", "Delay")),
         submitButton()
       ),
       mainPanel(
+        "If this panel is empty, that means the data is loading. Thank you for your patience!",
+        img(src='emoji.png', align = "center"),
         # "Please be patient while the decision tree results generate",
         # verbatimTextOutput("myData")
         plotOutput('myPlot')
